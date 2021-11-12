30e9d43b6a4caed79b11033b2efdfdde246f1366
Roberto Lopez-trigo <Bobby@Robertos-MacBook-Pro-2.local>
2021-11-11 22:13:30 -0500

    npm installed expo-av to record and play sounds, it works ;)

---
+++ frontend/components/AudioTest.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Audio } from 'expo-av';
+
+export default function AudioTest() {
+  const [recording, setRecording] = React.useState();
+  const [recordingList,setRecordingList] =React.useState([]);
+
+
+//   const addToRecordings=(songLocation)=>{
+//       recordingList.push(
+//           <Button
+//             title='song'
+//             onPress={playOrPause}
+//             ></Button>
+//       )
+//   }
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync(
+         Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI(); 
+    console.log('Recording stopped and stored at', uri);
+    // new Audio(uri).play()
+    let list =[...recordingList]
+    list.push(uri)
+    setRecordingList(list)
+
+
+
+    // const sound = new Audio.Sound()
+    // await sound.loadAsync({uri})
+    // await sound.playAsync()
+  }
+
+  const playOrPause= async (uri)=>{
+    const sound = new Audio.Sound()
+    await sound.loadAsync({uri})
+    await sound.playAsync()
+  }
+
+  return (
+    // <View style={styles.container}>
+    <View>
+      <Button
+        title={recording ? 'Stop Recording' : 'Start Recording'}
+        onPress={recording ? stopRecording : startRecording}
+      />
+      {recordingList.map(uri=><Button onPress={()=>playOrPause(uri)} title={uri}/>)}
+    </View>
+  );
+}
--- frontend/screens/TabOneScreen.tsx
+++ frontend/screens/TabOneScreen.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { StyleSheet } from 'react-native';
+import AudioTest from '../components/AudioTest';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -11,6 +12,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <AudioTest/>
     </View>
   );
 }
